eb812a31f939cdb3c9f64175225a65bfdc6a18b8
Igrey1321 <igrey1321@gmail.com>
2021-01-20 20:32:28 +0200

    End

--- js/script.js
+++ js/script.js
@@ -1,18 +1,6 @@
 $('.owl-carousel').owlCarousel({
   loop: true,
-  responsive: {
-    0: {
-      items: 1,
-      nav: true
-    },
-    600: {
-      items: 3,
-      nav: false
-    },
-    1000: {
-      items: 5,
-      nav: true,
-      loop: false
-    }
-  }
+  margin: 30,
+  autoplay: true,
+  items: 4,
 })
